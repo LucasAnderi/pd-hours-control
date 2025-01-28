e9d4fecf7aaec3e9ecbc274818a97d8f35448187
Lucas <luc.anderi22@gmail.com>
2025-01-28 09:02:35 -0300

    commit: Organizing packages

---
+++ pdhc-api/src/main/java/org/pd/pdhcapi/PdhcApiApplication.java
@@ -0,0 +1,13 @@
+package org.pd.pdhcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdhcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdhcApiApplication.class, args);
+    }
+
+}
